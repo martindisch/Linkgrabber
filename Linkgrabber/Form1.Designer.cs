bf8d9a8d8a766e3ae5a0f5a7b4dd31983e9b2710
Martin Disch <martindisch@gmail.com>
2015-01-18 11:57:39 +0100

    Technically working
    If only it wasn't for the crappy site showing the links with the wrong
    subtitle language.
    Looks like I'll have to grab the episode links from the overview page.
    Also, it would be great if we could have the whole thing threaded.

--- Linkgrabber/Form1.Designer.cs
+++ Linkgrabber/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // textBox1
@@ -59,16 +60,27 @@
             this.textBox2.Size = new System.Drawing.Size(960, 248);
             this.textBox2.TabIndex = 2;
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 306);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(960, 22);
+            this.progressBar1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 312);
+            this.ClientSize = new System.Drawing.Size(984, 337);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -80,6 +92,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- Linkgrabber/Form1.cs
+++ Linkgrabber/Form1.cs
@@ -21,6 +21,8 @@ namespace Linkgrabber
 
         private void button1_Click(object sender, EventArgs e)
         {
+            textBox2.Clear();
+
             string urlAddress = textBox1.Text;
 
             WebClient client = new WebClient();
@@ -28,6 +30,30 @@ namespace Linkgrabber
 
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
             doc.LoadHtml(htmlCode);
+
+            String hostUrl = new Uri(urlAddress).Host;
+
+            // Get episode count
+            htmlCode = client.DownloadString("http://" + hostUrl + "/" + doc.DocumentNode.SelectNodes("//a[@href]")[9].GetAttributeValue("href", "nothing"));
+            doc.LoadHtml(htmlCode);
+            HtmlNodeCollection coll = doc.DocumentNode.SelectNodes("//td[@class='epnum']");
+            HtmlNode last = coll[coll.Count - 1];
+            int episodeCount = Convert.ToInt32(last.InnerHtml.Replace(" ", ""));
+
+            progressBar1.Maximum = episodeCount + 1;
+            progressBar1.Step = 1;
+            progressBar1.Value = 1;
+
+            String nextAddress = urlAddress.Replace("http://", "").Replace(hostUrl, "");
+            for (int i = 0; i < episodeCount; i++)
+            {
+                doc.LoadHtml(client.DownloadString("http://" + hostUrl + "/" + nextAddress));
+                textBox2.Text += doc.DocumentNode.SelectNodes("//iframe[@src]")[0].GetAttributeValue("src", "nothing") + "\r\n";
+                nextAddress = doc.DocumentNode.SelectNodes("//a[@href]")[10].GetAttributeValue("href", "nothing");
+                progressBar1.PerformStep();
+            }
+
+            MessageBox.Show("All links grabbed", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
             // Get next episode
             //textBox2.Text = doc.DocumentNode.SelectNodes("//a[@href]")[10].GetAttributeValue("href", "nothing");
